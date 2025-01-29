94f7cac09f28b4f65be4282bbccf9b352ca34b25
Baibhav Kharel <kharelbaibhav7@gmail.com>
2025-01-29 15:03:03 +0545

    Check

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Signup from "./components/Signup"
+import App from './App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Signup />
+    <App />
   </StrictMode>,
 )
