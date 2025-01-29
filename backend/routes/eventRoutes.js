ee3c04581b17222b2d6ac359cb0a7d226afed27d
Baibhav Kharel <kharelbaibhav7@gmail.com>
2025-01-29 21:15:13 +0545

    Backend rewritein

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -1,8 +1,15 @@
 import express from "express";
 const router = express.Router();
-import { createEvent, getAllEvents } from "../controllers/eventController.js";
+import {
+  createEvent,
+  deleteEvent,
+  getAllEvents,
+  getEventById,
+} from "../controllers/eventController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 router.route("/").post(protect, createEvent).get(getAllEvents);
+
+router.route("/:id").delete(protect, deleteEvent).get(getEventById);
 
 export default router;
