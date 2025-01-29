bb17f28d8ef2be0e3d8858e694e7f73db2b6daae
DipsonShiwakoti <dipsonshiwakoti786@gmail.com>
2025-01-29 14:54:35 +0545

    Dipson

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Signup from "./components/Signup"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Signup />
   </StrictMode>,
 )
