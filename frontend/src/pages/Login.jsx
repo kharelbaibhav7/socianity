02c109735c0d187c27aee4fc7e80353f31abc6f0
Bhusan-Ghimire <ssssgggg550@gmail.com>
2025-01-29 15:53:40 +0545

    signin signup connected to Database

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,37 @@
+import React, {useState} from "react";
+
+const Login = () => {
+    let [data, setData] = useState({})
+    
+        let handleData = (e) => {
+            setData({
+            ...data, 
+            [e.target.name] : e.target.value
+            })
+        }
+    
+        let handleSubmit = async (e) => {
+            e.preventDefault()
+            try {
+                let result = await axios.post("http://localhost:8000/api/users/login", data);
+                localStorage.setItem("token", result.data.token);
+            } catch (error) {
+                console.log(error.message);
+            }        
+        }
+    return (
+        <div className="container">
+            <h2>Login</h2>
+            <form className="form" onSubmit={handleSubmit}>
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" name="email" placeholder="Enter your email" required onChange={handleData}/>
+
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" name="password" placeholder="Enter your password" required onChange={handleData}/>
+
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+};
+export default Login;
---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+
+const Signup = () => {
+    let [data, setData] = useState({})
+
+    let handleData = (e) => {
+        setData({
+        ...data, 
+        [e.target.name] : e.target.value
+        })
+    }
+
+    let handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            let result = await axios.post("http://localhost:8000/api/users/", data);
+            localStorage.setItem("token", result.data.token);
+        } catch (error) {
+            console.log(error.message);
+        }        
+    }
+
+    return (
+        <div className="container">
+            <h2>Sign Up</h2>
+
+            <form className="form" onSubmit={handleSubmit}>
+                <label htmlFor="fullName">Full Name</label>
+                <input type="text" id="fullName" name="fullName" placeholder="Enter your full name" required onChange={handleData} />
+
+                <label htmlFor="phone">Phone</label>
+                <input type="tel" id="phone" name="phoneNumber" placeholder="Enter your phone number" required onChange={handleData}/>
+
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" name="email" placeholder="Enter your email" required onChange={handleData}/>
+
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" name="password" placeholder="Enter your password" required onChange={handleData}/>
+
+                <button type="submit">Sign Up</button>
+            </form>
+        </div>
+    );
+};
+export default Signup;
