6b25fa6756294f7fe2c6dda78f4f2a2a5f17d796
Bhusan-Ghimire <ssssgggg550@gmail.com>
2025-01-30 16:52:32 +0545

    this is commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { createContext, useState } from 'react';
 import MyRoutes from './components/MyRoutes';
 import NavBar from './components/NavBar';
 import "./pages/styles.css";
+import "./App.css"
 
 
 
@@ -13,12 +14,12 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div>
       <GlobalVariableContext.Provider value={{ token: token, setToken: setToken }}>
         <NavBar />
         <MyRoutes />
       </GlobalVariableContext.Provider>
-    </>
+    </div>
   )
 }
 
