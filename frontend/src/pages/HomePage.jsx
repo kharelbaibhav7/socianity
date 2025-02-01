c0813b769a0f8f1bd56082277931f55109e1564e
Bhusan-Ghimire <ssssgggg550@gmail.com>
2025-02-01 15:22:58 +0545

    home page extended

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -7,7 +7,7 @@ const HomePage = () => {
             {/* Socianity Big font text part */}
             <section className="middlePage">
                 <div className="textContainer">
-                    <h1 className="main_title bold text-[3rem]">Socianity</h1>
+                    <h1 className="main_title font-bold text-[3rem]">Socianity</h1>
                     <h2 className="main_slogan text-[1.5rem]">Strengthening society, Uniting for Change</h2>
                     <div className="line h-[2px] w-[10vw] bg-emerald-400 mt-1"></div >
                 </div>
@@ -15,7 +15,7 @@ const HomePage = () => {
             </section>
 
             {/* Peoples */}
-            <div className="poeplesContainer absolute right-0 top-[5vh]">
+            <div className="poeplesContainer absolute right-7 top-[5vh]">
                 <img src="../../images/HomePagePeople.png" alt="" className='max-h-[70vh] w-auto' />
             </div>
 
@@ -27,7 +27,7 @@ const HomePage = () => {
                     <div className="imageContainer overflow-hidden">
                         <img src="../../images/EventsLogo.png" alt="" className="h-full w-auto grow" />
                     </div>
-                    <p className='title bold text-[1rem] text-center'>Events</p>
+                    <p className='title font-semibold text-[1rem] text-center'>Events</p>
                     <p className="description text-[0.7rem] text-center">Join inspiring community events </p>
                 </div>
 
@@ -36,7 +36,7 @@ const HomePage = () => {
                     <div className="imageContainer overflow-hidden">
                         <img src="../../images/DonationsLogo.png" alt="" className="h-full w-auto grow" />
                     </div>
-                    <p className='title bold text-[1rem] text-center'>Donations</p>
+                    <p className='title font-semibold text-[1rem] text-center'>Donations</p>
                     <p className="description text-[0.7rem] text-center">Make a donation to create an impact.</p>
                 </div>
 
@@ -45,7 +45,7 @@ const HomePage = () => {
                     <div className="imageContainer overflow-hidden">
                         <img src="../../images/ContributionsLogo.png" alt="" className="h-full w-auto grow" />
                     </div>
-                    <p className='title bold text-[1rem] sm:text-[0.8rem] text-center'>Contributions</p>
+                    <p className='title font-semibold text-[1rem] sm:text-[0.8rem] text-center'>Contributions</p>
                     <p className="description text-[0.7rem] text-center">Read abbout the great social contributors.</p>
                 </div>
 
@@ -54,23 +54,34 @@ const HomePage = () => {
                     <div className="imageContainer overflow-hidden">
                         <img src="../../images/ForumLogo.png" alt="" className="h-full w-auto grow" />
                     </div>
-                    <p className='title bold text-[1rem] text-center'>Forum</p>
+                    <p className='title font-semibold text-[1rem] text-center'>Forum</p>
                     <p className="description text-[0.7rem] text-center">Share your ideas and views for a stronger community.</p>
                 </div>
             </div>
 
             <div className="lowerRightBigText_And_ButtonContainer absolute top-[75vh] right-0 w-[35vw] flex flex-col items-center justify-between">
-                <p className="lowerRightBigText bold text-[1.5rem]">
+                <p className="lowerRightBigText font-semibold text-[1.5rem]">
                     Earn rewards while making a dierence-Join us today!
                 </p>
                 <button className='mt-[1rem] bg-amber-500 rounded-2xl p-2 text-2xl duration-250 hover:scale-110'>Let's Get Started</button>
             </div>
 
+            <div className="secondPageMainBox w-full h-[45vh] flex gap-[2rem] mt-[3rem] bg-white rounded-3xl justify-around align-center py-[1rem] px-[2rem]">
+                
+                    <div className="text_container flex flex-col justify-center items-left gap-[1rem]">
+                        <p className='font-bold text-blue-400 text-[1.5rem]'>ABOUT US</p>
+                        <p className='mainHeading text-[2.5rem] font-bold'>About socianity</p>
+                        <p className='font-normal'>Socianity Is a community-driven platform that unites individuals and organizations to create positive change. Through collaboration, contribution and meaningful connections, we empower people to strengthen society and build a better future together</p>
+                    </div>
+               
+                <img src="../../images/People.jpeg" alt="" className='rounded-sm scale-103'/>
+            </div>
 
-
-
-
-            {/* <button className="main_button">Get Started</button> */}
+            <div className="BottomBigTextContainer border border-white p-2 mt-10 mx-auto flex flex-col gap-[0.85rem] justify-center items-center">
+                <p className='text-[2rem] font-font-font-bold'>Ready to make a difference?</p>
+                <p className='font-normal text-center w-[50%]'>Join socianity and become part of a thriving community dedicated to positive change. Whether you want to participate in meaningful events, contribute to impactful projects, donate to support causes, or engage in decisions discussions, there's a place for you here.</p>
+                <button className='bg-blue-400 rounded-3xl py-3 px-9 border-none '>Join Now</button>
+            </div>
 
         </div>
     )
